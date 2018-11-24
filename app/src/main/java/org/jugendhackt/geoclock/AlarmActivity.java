174bf7e55f4092542da25474d33f2e12de3737fd
Jonas Sailer <jonas.huawei.sailer@gmail.com>
2018-11-24 15:25:43 +0100

    Klingeln hinzufügen
    Der Wecker kligelt! :+1:

--- app/src/main/java/org/jugendhackt/geoclock/AlarmActivity.java
+++ app/src/main/java/org/jugendhackt/geoclock/AlarmActivity.java
@@ -1,13 +1,33 @@
 package org.jugendhackt.geoclock;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlarmActivity extends AppCompatActivity {
-
+    Ringtone ringtone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm);
+        Uri uri=RingtoneManager.getActualDefaultRingtoneUri(this, RingtoneManager.TYPE_ALARM);
+        ringtone=RingtoneManager.getRingtone(this, uri);
+        ringtone.play();
+        Button button_stop=findViewById(R.id.button_stop);
+        button_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    @Override
+    public void onStop(){
+        ringtone.stop();
+        super.onStop();
     }
 }
