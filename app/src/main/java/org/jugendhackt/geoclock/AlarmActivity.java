7850cbe7dc079f525e76b7ec5e8c65da13b717d5
Jonas Sailer <jonas.huawei.sailer@gmail.com>
2018-11-24 19:26:32 +0100

    Revert "Revert "Klingelton einstellbar""
    This reverts commit 3dd835413a22b3f8e5b91a0362bdfe6d56bfb486.

--- app/src/main/java/org/jugendhackt/geoclock/AlarmActivity.java
+++ app/src/main/java/org/jugendhackt/geoclock/AlarmActivity.java
@@ -1,10 +1,12 @@
 package org.jugendhackt.geoclock;
 
+import android.content.SharedPreferences;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,9 +16,13 @@ public class AlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm);
-        Uri uri=RingtoneManager.getActualDefaultRingtoneUri(this, RingtoneManager.TYPE_ALARM);
-        ringtone=RingtoneManager.getRingtone(this, uri);
-        ringtone.play();
+        SharedPreferences einstellungen=PreferenceManager.getDefaultSharedPreferences(this);
+        String tonton=einstellungen.getString("tonton", null);
+        if (tonton!=null){
+            Uri uri=Uri.parse(tonton);
+            ringtone=RingtoneManager.getRingtone(this, uri);
+            ringtone.play();
+        }
         Button button_stop=findViewById(R.id.button_stop);
         button_stop.setOnClickListener(new View.OnClickListener() {
             @Override
