b925eddd0d76a48f6e44f12569f39cd6505c151c
NyanAlpaka <thegamersepp@gmail.com>
2018-11-25 10:22:33 +0100

    Auswahlkarte mit Pin
    Unfertig

--- app/src/main/java/org/jugendhackt/geoclock/MapActivity.java
+++ app/src/main/java/org/jugendhackt/geoclock/MapActivity.java
@@ -1,13 +1,88 @@
 package org.jugendhackt.geoclock;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.media.session.MediaSession;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.Projection;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
 
 public class MapActivity extends AppCompatActivity {
-
+   MapView Auswahlkarte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         setContentView(R.layout.activity_map);
+        Auswahlkarte = (MapView) findViewById(R.id.map);
+        Auswahlkarte.setTileSource(TileSourceFactory.MAPNIK);
+        Auswahlkarte.setBuiltInZoomControls(true);
+        Auswahlkarte.setMultiTouchControls(true);
+        MediaSession map;
+        IMapController mapController = Auswahlkarte.getController();
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(48.39651, 9.99042);
+        mapController.setCenter(startPoint);
+
+
+
+        Overlay touchOverlay = new Overlay(this){
+            ItemizedIconOverlay<OverlayItem> anotherItemizedIconOverlay = null;
+            @Override
+            public void draw(Canvas arg0, MapView arg1, boolean arg2) {
+
+            }
+            @Override
+            public boolean onSingleTapConfirmed(final MotionEvent e, final MapView mapView) {
+
+                final Drawable marker = getApplicationContext().getResources().getDrawable(R.drawable.ic_location_marker);
+                Projection proj = mapView.getProjection();
+                GeoPoint loc = (GeoPoint) proj.fromPixels((int)e.getX(), (int)e.getY());
+                String longitude = Double.toString(((double)loc.getLongitudeE6())/1000000);
+                String latitude = Double.toString(((double)loc.getLatitudeE6())/1000000);
+                System.out.println("- Latitude = " + latitude + ", Longitude = " + longitude );
+                ArrayList<OverlayItem> overlayArray = new ArrayList<OverlayItem>();
+                OverlayItem mapItem = new OverlayItem("", "", new GeoPoint((((double)loc.getLatitudeE6())/1000000), (((double)loc.getLongitudeE6())/1000000)));
+                mapItem.setMarker(marker);
+                overlayArray.add(mapItem);
+                if(anotherItemizedIconOverlay==null){
+                    anotherItemizedIconOverlay = new ItemizedIconOverlay<OverlayItem>(getApplicationContext(), overlayArray,null);
+                    mapView.getOverlays().add(anotherItemizedIconOverlay);
+                    mapView.invalidate();
+                }else{
+                    mapView.getOverlays().remove(anotherItemizedIconOverlay);
+                    mapView.invalidate();
+                    anotherItemizedIconOverlay = new ItemizedIconOverlay<OverlayItem>(getApplicationContext(), overlayArray,null);
+                    mapView.getOverlays().add(anotherItemizedIconOverlay);
+                }
+                //      dlgThread();
+                return true;
+            }
+        };
+        Auswahlkarte.getOverlays().add(touchOverlay);
+    }
+    public void onResume(){
+        super.onResume();
+        Auswahlkarte.onResume();
+    }
+
+    public void onPause(){
+        super.onPause();
+        Auswahlkarte.onPause();
     }
 }
